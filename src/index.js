cd1646ede134b85e0c2fcc46cac51ce8eafd655d
Sarah Hong <shong88tx@gmail.com>
2020-04-16 16:46:40 -0500

    added timer to reloading website

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+
+$('#main').html('Here we go!');
+let num = 0;
+setInterval(() => {
+    num = num + 1;
+    $('#main').text(`You've been on this page for ${num} seconds`);
+}, 1000);
