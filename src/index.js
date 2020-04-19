a6dbf26ae0d83364e31b743f8ee235c5ac667829
Sarah Hong <shong88tx@gmail.com>
2020-04-19 10:24:42 -0500

    sa4 added react to starterpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // change require to es6 import style
 import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 
 $('#main').html('Here we go!');
@@ -8,3 +10,6 @@ setInterval(() => {
   num += 1;
   $('#main').text(`You've been on this page for ${num} seconds`);
 }, 1000);
+
+const App = () => <div className="test">All the REACT belong to us!</div>;
+ReactDOM.render(<App />, document.getElementById('main'));
