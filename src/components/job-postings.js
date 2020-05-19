28fd50e3d848c58defd2803d51462a5ba2c1dbc3
Juliette Pouchol <juliette.a.pouchol.20@dartmouth.edu>
2020-05-19 18:59:48 -0400

    added components and routes for the nav

---
+++ src/components/job-postings.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const JobPostings = (props) => {
+  return (
+    <div>This is where job postings will go</div>
+  );
+};
+
+export default JobPostings;
---
+++ src/components/startups.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Startups = (props) => {
+  return (
+    <div>This is where startups will go</div>
+  );
+};
+
+export default Startups;
