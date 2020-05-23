f4dfd954df9b424dd409871694a891770735a65b
doniatung <donia.tung@dali.dartmouth.edu>
2020-05-23 13:45:09 -0400

    forgot to comment out route change

--- src/components/posting-item.js
+++ src/components/posting-item.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 
 const PostListItem = (props) => {
-  // const route = `/posts/${props.post._id}`;
-  const route = '/';
-  console.log(route);
+  const route = `/posts/${props.post._id}`;
+  // const route = '/';
+  // console.log(route);
   return (
     <div className="postListItem" key={props.post.id}>
       <Link to={route} key={props.post.id} className="link">
