f16d7d59f396a203a96a3a6d168c58e25c901521
doniatung <donia.tung@dali.dartmouth.edu>
2020-05-25 15:11:56 -0400

    styling for postings and startups

--- src/components/startup-item.js
+++ src/components/startup-item.js
@@ -5,11 +5,22 @@ import '../styles/postings.scss';
 const StartupListItem = (props) => {
   const route = `/startups/${props.startup._id}`;
   return (
-    <div className="listItem" key={props.startup.id}>
-      <Link to={route} key={props.startup.id} className="link">
-        <h1 className="startupName">{props.startup.name}</h1>
-      </Link>
-    </div>
+    <Link to={route} key={props.startup.id} className="listItem link">
+      <div className="companyInfo">
+        <img src={props.startup.logo} alt="no logo" />
+        <div className="companyText">
+          <h1 id="startupName"> {props.startup.name} </h1>
+          <div className="location">
+            <span className="locationIcon" />
+            <h2> {props.startup.location} </h2>
+          </div>
+        </div>
+      </div>
+      <div className="extraInfo">
+        <h1> Industries: {props.startup.industry} </h1>
+        <h2> Good fit for: </h2>
+      </div>
+    </Link>
   );
 };
 
