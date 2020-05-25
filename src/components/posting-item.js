7952cba6d36d0a71710cdb2bba4e38b290afa103
doniatung <donia.tung@dali.dartmouth.edu>
2020-05-25 15:05:19 -0400

    postings icon styled

--- src/components/posting-item.js
+++ src/components/posting-item.js
@@ -7,16 +7,15 @@ import '../styles/postings.scss';
 const PostListItem = (props) => {
   const route = `/posts/${props.post._id}`;
   return (
-  // <div  key={props.post.id}>
     <Link to={route} key={props.post.id} className="listItem link">
       <div className="companyInfo">
         <img src={props.startup.logo} alt="no logo" />
         <div className="companyText">
           <h1 id="startupName"> {props.startup.name} </h1>
-          <h2>
+          <div className="location">
             <span className="locationIcon" />
-            {props.startup.location}
-          </h2>
+            <h2> {props.startup.location} </h2>
+          </div>
         </div>
       </div>
       <div className="postInfo">
@@ -24,7 +23,6 @@ const PostListItem = (props) => {
         <h2 id="matched">Matched on: </h2>
       </div>
     </Link>
-  // </div>
   );
 };
 
