d3f359871df630e3976d1491bcb4c7719249aa76
Sarah Hong <shong88tx@gmail.com>
2020-06-03 09:55:53 -0500

    adding questions to addProfile

--- src/components/startup-components/startup-profile-startups.js
+++ src/components/startup-components/startup-profile-startups.js
@@ -2,21 +2,35 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { fetchStartupByUserID, fetchPosts, fetchPost } from '../../actions/index';
+import AddPosting from './startups-modals/startup-add-posting';
 import '../../styles/startup-profile.scss';
 
 class StartupProfile extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      show: false,
     };
-    this.renderPostings = this.renderPostings.bind(this);
+    // this.renderPostings = this.renderPostings.bind(this);
   }
 
   componentDidMount() {
     console.log('didMount');
     this.props.fetchStartupByUserID(localStorage.getItem('userID'));
-    this.props.fetchPosts();
+    // this.props.fetchPosts();
   }
+
+  showModal = () => {
+    this.setState({
+      show: true,
+    });
+  };
+
+  hideModal = () => {
+    this.setState({
+      show: false,
+    });
+  };
 
   renderDescription() {
     if (this.post.description.length > 100) {
@@ -31,7 +45,7 @@ class StartupProfile extends Component {
     }
   }
 
-  renderPostings() {
+  renderPostings = (e) => {
     if (this.props.posts && this.props.posts.length && typeof this.props.startup !== 'undefined') {
       const mappingPostings = this.props.startup.posts.map((postID) => {
         this.post = this.props.posts.find((x) => x.id === postID);
@@ -49,7 +63,7 @@ class StartupProfile extends Component {
         this.props.posts !== undefined
           ? (
             <div className="startup-postings">
-              <h1>Internship Postings:</h1>
+              <h1>Volunteer Positions:</h1>
               <ul className="startup-postings-list">
                 {mappingPostings}
               </ul>
@@ -58,7 +72,20 @@ class StartupProfile extends Component {
       );
     } else {
       return (
-        <div>posts are undefined</div>
+        <div className="startup-postings">
+          <h1 className="startup-postings-h1">Volunteer Positions:</h1>
+          <div className="btn-box">
+            <button type="button"
+              className="startup-add-posting-btn"
+              onClick={() => {
+                this.showModal();
+              }}
+            >
+              <i className="fas fa-plus" />
+            </button>
+          </div>
+        </div>
+        /* <div>posts are undefined</div> */
       );
     }
   }
@@ -77,6 +104,7 @@ class StartupProfile extends Component {
     if (typeof this.props.startup !== 'undefined') {
       return (
         <div className="startup-body">
+          <AddPosting onClose={this.hideModal} show={this.state.show} />
           <h1 className="startup-name">{`${this.props.startup.name}`}</h1>
           <div className="startup-location">Location: {`${this.props.startup.location}`}</div>
           {/* <div className="startup-industries"><div>Industry: </div>{this.renderIndustries()}</div> */}
@@ -115,7 +143,7 @@ class StartupProfile extends Component {
 function mapStateToProps(reduxState) {
   return {
     startup: reduxState.startups.current,
-    posts: reduxState.posts.all,
+    // posts: reduxState.posts.all,
   };
 }
 
