4f4000ab380190306ca46b8433909f0d436accf1
doniatung <donia.tung@dali.dartmouth.edu>
2020-06-04 15:26:46 -0400

    before pulling master

--- src/services/datastore.js
+++ src/services/datastore.js
@@ -1,8 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
-// const ROOT_URL = 'http://localhost:9090/api';
-const ROOT_URL = 'http://project-mcv.herokuapp.com/api';
+const ROOT_URL = 'http://localhost:9090/api';
+// const ROOT_URL = 'http://project-mcv.herokuapp.com/api';
 
 export const fetchIndustriesFromID = (idArray, callback) => {
   axios.get(`${ROOT_URL}/industries/${idArray}`, { headers: { authorization: localStorage.getItem('token') } }).then((response) => {
