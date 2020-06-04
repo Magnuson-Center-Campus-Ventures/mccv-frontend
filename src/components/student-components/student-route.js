01582c6eb20319dc448caf4f9148e0b83d3a16ea
dmw21 <Dylan.M.Whang.21@dartmouth.edu>
2020-06-04 19:29:56 -0400

    added other routes

---
+++ src/components/student-components/student-route.js
@@ -0,0 +1,22 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Route, Redirect } from 'react-router-dom';
+import { fetchUser } from '../../actions';
+
+const StudentRoute = ({ component: Child, ...props }) => {
+  return (
+    <Route {...props}
+      render={(routeProps) => (props.authenticated && props.role === 'student' ? (<Child {...routeProps} />) : (<Redirect to="/" />))}
+    />
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    authenticated: state.auth.authenticated,
+    role: state.user.current.role,
+  };
+};
+
+export default withRouter(connect(mapStateToProps, { fetchUser })(StudentRoute));
