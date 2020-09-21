d54e5c9c7db218e7212fa19a2cb816c7ca011f23
dmw21 <Dylan.M.Whang.21@dartmouth.edu>
2020-09-21 01:53:17 -0400

    added conditional render for nav and foot on signup sequence pages

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 import '../styles/footer.scss';
 import logo from '../assets/magnuson_logo.png';
 
-const Footer = () => {
-  return (
-    <ul id="footer">
-      <li>
-        <a href="https://magnuson.dartmouth.edu/">
-          <img
-            src={logo}
-            alt="Magnuson Logo"
-            style={{ cursor: 'pointer' }}
-          />
-        </a>
-      </li>
-      <li><NavLink to="/credits">Credit</NavLink></li>
-      <li><a href="mailto:magnuson.student.leadership.board@dartmouth.edu">Contact</a></li>
-    </ul>
-  );
+class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true,
+    };
+  }
+
+  componentDidUpdate() {
+    if (window.location.href.indexOf("startup-signup") > -1 || window.location.href.indexOf("student-signup") > -1) {
+      this.state.show = false;
+    } else {
+      this.state.show = true;
+    }
+  }
+
+  render() {
+    console.log(this.state.show);
+    return this.state.show ? (
+      <ul id="footer">
+        <li>
+          <a href="https://magnuson.dartmouth.edu/">
+            <img
+              src={logo}
+              alt="Magnuson Logo"
+              style={{ cursor: 'pointer' }}
+            />
+          </a>
+        </li>
+        <li><NavLink to="/credits">Credit</NavLink></li>
+        <li><a href="mailto:magnuson.student.leadership.board@dartmouth.edu">Contact</a></li>
+      </ul>
+    )
+    : (<nav />);
+  }
 };
 
-export default Footer;
+const mapStateToProps = (reduxState) => ({
+  user: reduxState.user.current,
+});
+
+export default withRouter(connect(mapStateToProps)(Footer));
