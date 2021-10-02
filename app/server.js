f25bf05dbffc75a4a90e81af7db7a3fcaa8c1c18
FudgeDaMuffin <juliancgeorge@gmail.com>
2021-10-02 18:22:44 -0400

    Further debugging

--- app/server.js
+++ app/server.js
@@ -3,27 +3,37 @@ const { spawn } = require('child_process');
 const express = require('express');
 
 const app = express();
-console.log(process.env.PRODUCTION)
 if (process.env.PRODUCTION === "True") {
-  console.log("PROD")
+  const child = spawn('yarn',['build']);
+  child.stdout.setEncoding('utf8');
+  child.stdout.on('data', (chunk) => {
+    console.log(chunk);
+  });
+  child.on('error', (err)=>{
+    console.log("Error while initiating production server: \n",err);
+  });
+  child.on('close', (code) => {
+    console.log(`Production server process exited with code ${code}`);
+  });
   app.use(express.static(path.join(__dirname, 'dist')));
 
-  app.set('PORT', process.env.PORT);
-  app.set('HOST', '0.0.0.0');
+  let port = process.env.PORT || 3000;
+  let host = '0.0.0.0'
 
-  const server = app.listen(app.get('PORT'),app.get('HOST'), function() {
-    console.log('Production server listening on port', server.address().port);
+  const server = app.listen(port,host, function() {
+    console.log('Production server listening on port', port);
   });
 }
 else {
     console.log("Initiating development server... \n");
-    const child = spawn('yarn',['dev']);
+    const child = spawn('yarn',['dev'], {shell: true});
     child.stdout.setEncoding('utf8');
     child.stdout.on('data', (chunk) => {
       console.log(chunk);
     });
     child.on('error', (err)=>{
       console.log("Error while initiating development server: \n",err);
+      throw err
     });
     child.on('close', (code) => {
       console.log(`Development server process exited with code ${code}`);
