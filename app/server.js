c87dd1f58bdeca68eee119e9363bbf5eb4a1ca4b
FudgeDaMuffin <juliancgeorge@gmail.com>
2021-10-02 21:37:37 -0400

    Debugging by checking file structure

--- app/server.js
+++ app/server.js
@@ -1,28 +1,41 @@
 const path = require('path');
+const fs = require('fs')
 const { spawn } = require('child_process');
 const express = require('express');
 
 const app = express();
 if (process.env.PRODUCTION === "True") {
-  // const child = spawn('yarn',['build']);
-  // child.stdout.setEncoding('utf8');
-  // child.stdout.on('data', (chunk) => {
-  //   console.log(chunk);
-  // });
-  // child.on('error', (err)=>{
-  //   console.log("Error while initiating production server: \n",err);
-  // });
-  // child.on('close', (code) => {
-  //   console.log(`Production server process exited with code ${code}`);
-  // });
-  app.use(express.static(path.join(__dirname, 'dist')));
-
-  let port = process.env.PORT || 3000;
-  let host = '0.0.0.0'
-
-  const server = app.listen(port,host, function() {
-    console.log('Production server listening on port', port);
+  const child = spawn('yarn',['heroku-postbuild']);
+  child.stdout.setEncoding('utf8');
+  child.stdout.on('data', (chunk) => {
+    console.log(chunk);
   });
+  child.on('error', (err)=>{
+    console.log("Error while initiating production server: \n",err);
+  });
+  child.on('close', (code) => {
+    console.log(`Production server process exited with code ${code}`);
+    let port = process.env.PORT || 3000;
+    let host = '0.0.0.0'
+    app.use(express.static(path.join(__dirname, 'dist')));
+    console.log(__dirname)
+    fs.readdir(__dirname, function (err, files) {
+      //handling error
+      if (err) {
+          return console.log('Unable to scan directory: ' + err);
+      } 
+      //listing all files using forEach
+      files.forEach(function (file) {
+          // Do whatever you want to do with the file
+          console.log(file); 
+      });
+    });
+    const server = app.listen(port,host, function() {
+      console.log('Production server listening on port', port);
+    });
+  });
+  
+  
 }
 else {
     console.log("Initiating development server... \n");
